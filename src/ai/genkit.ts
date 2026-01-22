6b66b766a970bf4a3efd8b3ed2520fdd24980b9f
Vino Chelo <vinochelo1986@gmail.com>
2026-01-22 16:16:46 +0000

    TAMBIEN PODEMOS HACER CAMBIOS EN LA VERSION DE API

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,7 +3,7 @@ import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [
-    googleAI({apiVersion: 'v1'}),
+    googleAI({apiVersion: 'v1beta'}),
   ],
   logLevel: 'debug',
   enableTracing: true,
