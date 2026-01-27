cfa2b42dd673a75fca3891a6584698de8f775716
Vino Chelo <vinochelo1986@gmail.com>
2026-01-27 19:35:50 +0000

    hay mucho espacio en material proveedor fecha reducelo para aumentar en

--- src/components/excel-processor-form.tsx
+++ src/components/excel-processor-form.tsx
@@ -331,7 +331,7 @@ export function ExcelProcessorForm() {
             }
         });
         
-        const columnWidths = [55, 65, 140, 55, 110, 135, 45, 45, 50, 50, 50, 50, 45];
+        const columnWidths = [55, 60, 155, 50, 90, 150, 45, 45, 50, 50, 50, 50, 45];
         const tableWidth = columnWidths.reduce((a, b) => a + b, 0);
         const scale = availableWidth / tableWidth;
         const scaledWidths = columnWidths.map(w => w * scale);
